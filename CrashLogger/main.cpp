021568471d0d4e67c13df958329d06e77380be35
Brandon <brandonahimes@gmail.com>
2024-12-04 20:42:48 -0700

    updated version number

--- CrashLogger/main.cpp
+++ CrashLogger/main.cpp
@@ -13,7 +13,7 @@ extern "C" {
       // fill out the info structure
       info->infoVersion = PluginInfo::kInfoVersion;
       info->name        = "CrashLoggerImproved";
-      info->version     = 0x01000000; // major, minor, patch, build
+      info->version     = 0x01030000; // major, minor, patch, build
 
       {  // log our version number -- be helpful!
          auto v = info->version;
