6ab8fb8c42d37481a8fb4fd8603aa64090913042
Brandon <brandonahimes@gmail.com>
2026-09-10 01:46:00 -0700

    removed GameData include

--- CrashLogger/Formatter.hpp
+++ CrashLogger/Formatter.hpp
@@ -2,7 +2,6 @@
 #include <format>
 #include <set>
 #include "GameObjects.h"
-#include "GameData.h"
 #include "GameMagicEffects.h"
 #include "Script.h"
 #include "GameTasks.h"
@@ -11,6 +10,210 @@
 
 // If class is described by a single line, no need to name the variable
 // If there is a member class, if it's one-line, leave it as one-line, if there are several, prepend the name and add offset
+
+class BSFile
+{
+public:
+	BSFile();
+	~BSFile();
+
+	virtual void	Destructor(bool freeMemory);				// 00
+	virtual void	Unk_01(void);								// 04
+	virtual void	Unk_02(void);								// 08
+	virtual void	Seek(SInt32 offset, UInt32 origin); //(void);								// 0C
+	virtual void	Unk_04(void);								// 10
+	virtual void	DumpAttributes(NiTArray <char*>* dst);	// 14
+	virtual UInt32	GetSize(void);								// 18
+	virtual void	Unk_07(void);								// 1C  //Check SkyBSA def.
+	virtual void	Unk_08(void);								// 20
+	virtual void	Unk_09(void);								// 24
+	virtual void	Unk_0A(void);								// 28
+	virtual void	Unk_0B(void);								// 2C
+	virtual void	Unk_0C(void);								// 30
+	virtual void	Unk_Read(void* destination, UInt32 sizeToRead);								// 34
+	virtual void	Unk_Write(void);							// 38
+
+	//	void	** m_vtbl;		// 000
+	void* m_readProc;	// 004 - function pointer
+	void* m_writeProc;	// 008 - function pointer
+	UInt32	m_bufSize;		// 00C
+	UInt32 lastReadSize; // 010 // return value of fread, fread_s, and friends, when we're reading into our own buffer
+	UInt32	m_unk014;		// 014
+	void* m_buf;		// 018
+	FILE* m_file;		// 01C
+	UInt32	m_writeAccess;	// 020
+	UInt8	m_good;			// 024
+	UInt8	m_pad025[3];	// 025
+	UInt8	m_unk028;		// 028
+	UInt8	m_pad029[3];	// 029
+	UInt32	m_unk02C;		// 02C
+	UInt32	m_pos;			// 030
+	UInt32	m_unk034;		// 034
+	UInt32	m_unk038;		// 038
+	char	m_path[0x104];	// 03C
+	UInt32	m_unk140;		// 140
+	UInt32	m_unk144;		// 144
+	UInt32	m_pos2;			// 148 - used if m_pos is 0xFFFFFFFF
+	UInt32	m_unk14C;		// 14C
+	UInt32	m_fileSize;		// 150
+};
+
+struct ModEntry
+{
+	// 41C / 420
+	struct Data		// referred to as 'TESFile' by Bethesda
+	{
+		enum
+		{
+			kFlag_IsMaster = 1 << 0,
+			kFlag_Loaded = 1 << 2,
+			kFlag_Active = 1 << 3
+		};
+
+		struct	ChunkInfo
+		{
+			UInt32	type;		// e.g. 'GRUP', 'GLOB', etc
+			UInt32	length;
+		};
+
+		struct	RecordInfo
+		{
+			ChunkInfo		chunkInfo;
+			UInt32			flags;
+			UInt32			recordID;
+			TrackingData	trackingData;
+		};
+		class GroupInfo : public RecordInfo
+		{// size 18/18
+			//     /*00*/ RecordInfo    // for group records, the size includes the 14 bytes of the header
+			UInt32        recordOffset;   // used internally to track header offsets of all open groups
+		};
+
+		struct  SizeInfo
+		{
+			UInt32		fileSizeHigh;			// WIN32_FIND_DATA::nFileSizeHigh
+			UInt32		fileSizeLow;			// WIN32_FIND_DATA::nFileSizeLow
+		};
+
+		// static members: B33C1C, B33C20
+		typedef BSSimpleList<GroupInfo*> GroupList;
+
+		UInt32	errorState;							// 000 appears to indicate status of file (open, closed, etc) 2, 9, 0C do stuff
+		UInt32	ghostFileParent;							// 004
+		UInt32	childThreadGhostFiles;							// 008
+		BSFile* unkFile00C;							// 00C
+		BSFile* bsFile;						// 010
+		UInt32	unk014;							// 014
+		UInt32	unk018;							// 018
+		char	name[0x104];					// 01C
+		char	filepath[0x104];				// 120 relative to "Oblivion\"
+		UInt32	unk224;							// 224
+		UInt32	unk228;							// 228 init to *(0xB055CC), seen 0x2800
+		UInt32	unk22C[(0x23C - 0x22C) >> 2];	// 22C
+		RecordInfo	currentRecordInfo;					// 23C
+		ChunkInfo	currentChunk;					// 250
+		UInt32               fileSize; // same as FileSizeLow in find data
+		UInt32               currentRecordOffset; // offset of current record in file
+		UInt32               currentChunkOffset; // offset of current chunk in record
+		UInt32               fetchedChunkDataSize; // number of bytes read in last GetChunkData() call
+		GroupInfo            unkFile268; // used when saving empty form records, e.g. for deleted forms
+		UInt32               unkFile280; // used when saving empty form records, e.g. for deleted forms  //280
+		GroupList            openGroups; // stack of open group records, from lowest level to highest //284
+		bool                 headerRead; // set after header has been successfully parsed //28C
+		UInt8                padFile28D[3];
+		WIN32_FIND_DATA	findData;				// 290
+		UInt32	version;						// 3D0 plugin version (0.8/1.0)
+		UInt32	formCount;						// 3D4 record/form count
+		UInt32	nextFormID;						// 3D8 used by TESFile::sub_486BF0 in the editor
+		UInt32	flags;							// 3DC
+		tList<char>		masterList;				// 3E0 linked list of .esm dependencies
+		tList<SizeInfo>	masterSizeInfo;			// 3E8 linked list of file size info for above list
+		UInt32	idx;							// 3F0 //This seems to represent the index of the current file (equals to the size of the master counts )
+		void* unk3F4;						// 3F4
+		UInt32	unk3F8;							// 3F8
+		UInt32	unk3FC;							// 3FC
+		UInt8	unk400;							// 400 init to -1
+		UInt8	pad401[3];
+		BSStringT	authorName;						// 404
+		BSStringT	modDescription;					// 40C
+
+		void* currentRecordDCBuffer;		 // buffer for decompressed record data //414
+		UInt32 currentRecordDCLength; // length of decompressed record data //418
+		//TESFile*             unkFile41C; // file this object was cloned from. used for local copies of network files?  COEF also report this member increasing the max size
+	};
+
+	Data* data;
+	ModEntry* next;
+
+	ModEntry* Next() const { return next; }
+	Data* Info() const { return data; }
+	bool IsLoaded()	const { return (data && (data->flags & Data::kFlag_Loaded)) ? true : false; }
+};
+
+struct TESRegionDataManager;
+
+class DataHandler
+{
+public:
+	DataHandler();
+	~DataHandler();
+
+	struct _Unk8B8											// as seen in the editor
+	{
+		UInt32				unk00;							// 00
+		UInt32				unk04;							// 04
+		UInt32				unk08;							// 08 initialized to (numLoadedMods << 24) | 0x800 during plugin load
+		ModEntry::Data* activeFile;						// 0C active plugin
+	};
+
+	BoundObjectListHead* boundObjects;					// 000
+	tList<TESPackage>		packages;						// 004
+	tList<TESWorldSpace>	worldSpaces;					// 00C
+	tList<TESClimate>		climates;						// 014
+	tList<TESWeather>		weathers;						// 01C
+	tList<EnchantmentItem>	enchantmentItems;				// 024
+	tList<SpellItem>		spellitems;						// 02C
+	tList<TESHair>			hairs;							// 034
+	tList<TESEyes>			eyes;							// 03C
+	tList<TESRace>			races;							// 044
+	tList<TESLandTexture>	landTextures;					// 04C
+	tList<TESClass>			classes;						// 054
+	tList<TESFaction>		factions;						// 05C
+	tList<Script>			scripts;						// 064
+	tList<TESSound>			sounds;							// 06C
+	tList<TESGlobal>		globals;						// 074
+	tList<TESTopic>			topics;							// 07C
+	tList<TESQuest>			quests;							// 084
+	tList<BirthSign>		birthsigns;						// 08C
+	tList<TESCombatStyle>	combatStyles;					// 094
+	tList<TESLoadScreen>	loadScreens;					// 09C
+	tList<TESWaterForm>		waterForms;						// 0A4
+	tList<TESEffectShader>	effectShaders;					// 0AC
+	tList<TESObjectANIO>	objectAnios;					// 0B4
+	TESRegionList* regionList;					// 0BC
+	NiTLargeArray <TESObjectCELL*>	cellArray;				// 0C0
+	//	UInt32					unk0D0[2];						// 0D0
+	TESSkill				skills[0x15];					// 0D8
+	_Unk8B8					unk8B8;							// 8B8
+	ModEntry				modList;						// 8C8
+	UInt32					numLoadedMods;					// 8D0
+	ModEntry::Data* modsByID[0xFF];				// 8D4
+	UInt32					unkCD0[(0xCD8 - 0xCD0) >> 2];	// CD0
+	TESRegionDataManager* regionDataManager;			// CD8
+	void* unkCDC;							// CDC //ptr to ExtraData?
+
+	bool	ConstructObject(ModEntry::Data* tesFile, bool unk1);
+
+	const ModEntry* LookupModByName(const char* modName);
+	const ModEntry** GetActiveModList();		// returns array of modEntry* corresponding to loaded mods sorted by mod index
+	UInt8 GetModIndex(const char* modName);
+	UInt8 GetActiveModCount();
+	const char* GetNthModName(UInt32 modIndex);
+	TESGlobal* GetGlobalVarByName(const char* varName, UInt32 nameLen);
+	TESQuest* GetQuestByEditorName(const char* questName, UInt32 nameLen = -1);
+	UInt32 GetFormIDBase(const char* modName);
+	const char* GetModNameForFormID(UInt32 formID);
+};
 
 inline auto Offset(std::vector<std::string> vector)
 {
